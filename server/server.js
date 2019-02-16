305f626e811d5fa3bff2f794867fd08703a4729d
HMW <howardmwiener@gmail.com>
2019-02-16 14:10:29 -0500

    reconfigured for Heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 
 app.post('/todos', (req, res) => {
   var todos = new Todos({
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
